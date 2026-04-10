f4fd09e0c8ab0faa4eb07f2875b406a897a710fb
mfsi.garimas <mfsi.garimas@gmail.com>
2026-04-10 17:25:36 +0530

    add install.sh for paddle

---
+++ backend/install.sh
@@ -0,0 +1,4 @@
+python -m pip install -r requirements.txt
+
+python -m pip install paddlepaddle==3.2.0 -i https://www.paddlepaddle.org.cn/packages/stable/cpu/
+python -m pip install "paddleocr[all]"
